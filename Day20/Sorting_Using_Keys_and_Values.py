6f15421031a31bdbe10087b67a4c293be9a7ac53
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-01-28 01:56:06 +0530

    Sorting_Using_Keys_and_Values

--- Day20/Sorting_Using_Keys_and_Values.py
+++ Day20/Sorting_Using_Keys_and_Values.py
@@ -1,27 +1,20 @@
-
 def Sorting_by_Keys(Key_Values):
     return {key: value for key, value in sorted(Key_Values.items())}
 
-
-Key_Values = {2: 56, 1: 2, 5: 12, 4: 24, 6: 18, 3: 323}
-print(Sorting_by_Keys(Key_Values))
-
-# Approach 1:
 
 def Sorting_by_Values(Key_Values):
     return {key: value for key, value in sorted(Key_Values.items(), key=lambda elements: elements[1])}
 
 
-Key_Values={2: 56, 1: 2, 5: 12, 4: 24, 6: 18, 3: 323}
-print(Sorting_by_Values(Key_Values))
-
-
-# Approach 2:
-
-from operator import itemgetter
-def Sorting_by_Values(Key_Values):
-    return {key: value for key, value in sorted(Key_Values.items(), key=itemgetter(1))}
-
-
 Key_Values = {2: 56, 1: 2, 5: 12, 4: 24, 6: 18, 3: 323}
-print(Sorting_by_Values(Key_Values))
+print(f"Sorting by Keys: {Sorting_by_Keys(Key_Values)}")
+print(f"Sorting by Values: {Sorting_by_Values(Key_Values)}")
+
+
+# from operator import itemgetter
+# def Sorting_by_Values(Key_Values):
+#     return {key: value for key, value in sorted(Key_Values.items(), key=itemgetter(1))}
+
+
+# Key_Values = {2: 56, 1: 2, 5: 12, 4: 24, 6: 18, 3: 323}
+# print(Sorting_by_Values(Key_Values))
