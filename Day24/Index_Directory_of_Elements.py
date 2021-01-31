07a65df84138e239405b7aad836685a8557a694c
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-02-01 01:14:08 +0530

    Update Index_Directory_of_Elements.py

--- Day24/Index_Directory_of_Elements.py
+++ Day24/Index_Directory_of_Elements.py
@@ -1,3 +1,5 @@
+# Approach 1:
+
 from collections import defaultdict
 
 
@@ -13,6 +15,7 @@ def Index_Directory(Test_list):
 Test_list = [7, 6, 3, 7, 8, 3, 6, 7, 8]
 print(Index_Directory(Test_list))
 
+# Approach 2:
 
 def Index_Directory(Test_list):
     return {key: [index for index, value in enumerate(Test_list) if value == key] for key in set(Test_list)}
