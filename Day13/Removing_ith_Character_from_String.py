776e532d4a097896f414b241b26bd17caf659305
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-02-03 00:27:48 +0530

    Delete Day13 directory

--- Day13/Removing_ith_Character_from_String.py
+++
@@ -1,7 +0,0 @@
-def Remove_ith_Character(Test_string, Character):
-    return Test_string[:Character]+Test_string[Character+1:]
-
-
-Test_string = input("Enter a String: ")
-Character = int(input("Enter ith Character: "))
-print(Remove_ith_Character(Test_string, Character))
