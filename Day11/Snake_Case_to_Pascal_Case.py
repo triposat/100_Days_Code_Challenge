6a447f1146c15fb4cb8ba1c6ada5fa1c494d95de
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-02-03 00:28:07 +0530

    Delete Day11 directory

--- Day11/Snake_Case_to_Pascal_Case.py
+++
@@ -1,6 +0,0 @@
-def Snake_to_Pascal(Test_String):
-    return Test_String.replace("_", " ").title().replace(" ", "")
-
-
-Test_String = input("Enter a String: ")
-print(Snake_to_Pascal(Test_String))
