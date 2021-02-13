9cf393ed293ab285870db5826b06eae54038bf77
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-02-14 00:11:41 +0530

    Add files via upload

---
+++ DAYS/Day39/Current_Time.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+Time_now = datetime.now()
+Result_time = Time_now.strftime("%H:%M:%S")
+print(Result_time)
---
+++ DAYS/Day39/Current_Time_and_Date.py
@@ -0,0 +1,4 @@
+import datetime
+Result = datetime.datetime.now()
+print(f"{Result.day}-{Result.month}-{Result.year}")
+print(f"{Result.hour}:{Result.minute}:{Result.second}")
