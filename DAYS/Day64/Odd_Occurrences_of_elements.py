691e8e8607d0e2aaf30071aadb44e6ebbdee00b8
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-03-28 16:55:38 +0530

    Update Odd_Occurrences_of_elements.py

--- DAYS/Day64/Odd_Occurrences_of_elements.py
+++ DAYS/Day64/Odd_Occurrences_of_elements.py
@@ -1,26 +1,26 @@
 # Approach 1:
 
 
-# def get_Odd_Occurrence(Test_list):
-#     a = set(Test_list)
-#     hey = {i: Test_list.count(i) for i in a}
-#     return [key for key, values in hey.items() if values % 2 != 0]
+def get_Odd_Occurrence(Test_list):
+    a = set(Test_list)
+    hey = {i: Test_list.count(i) for i in a}
+    return [key for key, values in hey.items() if values % 2 != 0]
 
 
-# Test_list = [4, 5, 4, 5, 2, 2, 3, 3, 2, 4, 4]
+Test_list = [4, 5, 4, 5, 2, 2, 3, 3, 2, 4, 4]
 
-# print(*get_Odd_Occurrence(Test_list))
+print(*get_Odd_Occurrence(Test_list))
 
 
 # Approach 2:
 
 
-# def get_Odd_Occurrence(Test_list):
-#     Result = 0
-#     for ele in Test_list:
-#         Result ^= ele
-#     return Result
+def get_Odd_Occurrence(Test_list):
+    Result = 0
+    for ele in Test_list:
+        Result ^= ele
+    return Result
 
 
-# Test_list = [4, 5, 4, 5, 2, 2, 3, 3, 2, 4, 4]
-# print(get_Odd_Occurrence(Test_list))
+Test_list = [4, 5, 4, 5, 2, 2, 3, 3, 2, 4, 4]
+print(get_Odd_Occurrence(Test_list))
